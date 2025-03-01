798fc7bdcb0ef7e7a26f474d93c1132c586a1607
Swaraj9 <swaraj.dusane90@gmail.com>
2025-03-01 15:14:18 +0530

    Database Connection

--- controllers/mobile/posts.controller.js
+++ controllers/mobile/posts.controller.js
@@ -1,13 +1,21 @@
+const { QueryTypes } = require("sequelize");
+const sequelize = require("../../config/database");
+
 const showPosts = async (req, res) => {
   try {
-    console.log("Test");
-    res.status(200).json({ message: "Showing Posts" });
+    const posts = await sequelize.query("SELECT * FROM posts", {
+      type: QueryTypes.SELECT,
+    });
+
+    res.status(200).json({ success: true, data: posts });
   } catch (err) {
     console.log(err);
-    res.status(500).json({ message: "Error While showing Posts" });
+    res
+      .status(500)
+      .json({ success: false, message: "Error While showing Posts" });
   }
 };
 
 module.exports = {
-    showPosts
+  showPosts,
 };
